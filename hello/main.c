7a724a281ce8e7ee244f8e3efc3448c5c82d41bc
Albeirton <47571759+Albeirton@users.noreply.github.com>
2019-02-12 16:57:29 -0200

    UM PROGRAMA DE SOMA E MUTICAÇAO

---
+++ hello/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+printf("DIGITE UM NUMERO QUALQUER\n");
+scanf("%d",&a);
+scanf("%d",&b);
+
+    c = a*b;
+
+
+    printf("resultado %d\n\n",c);
+    return 0;
+}
